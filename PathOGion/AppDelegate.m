62661a755ea875575fceae21a4a88c6ed3242577
Simon Ayzman <simonayzman@gmail.com>
2015-03-02 18:53:25 -0500

    Updated project with initial location tracking code

--- PathOGion/AppDelegate.m
+++ PathOGion/AppDelegate.m
@@ -15,8 +15,37 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    UIAlertView * alert;
+    //We have to make sure that the Background App Refresh is enable for the Location updates to work in the background.
+    if([[UIApplication sharedApplication] backgroundRefreshStatus] == UIBackgroundRefreshStatusDenied)
+    {
+        
+        alert = [[UIAlertView alloc]initWithTitle:@""
+                                          message:@"The app does not work without the Background App Refresh enabled. To turn it on, go to Settings > General > Background App Refresh"
+                                         delegate:nil
+                                cancelButtonTitle:@"Ok"
+                                otherButtonTitles:nil, nil];
+        [alert show];
+        
+    }
+    else if([[UIApplication sharedApplication] backgroundRefreshStatus] == UIBackgroundRefreshStatusRestricted)
+    {
+        alert = [[UIAlertView alloc]initWithTitle:@""
+                                          message:@"The functions of this app are limited because the Background App Refresh is disable."
+                                         delegate:nil
+                                cancelButtonTitle:@"Ok"
+                                otherButtonTitles:nil, nil];
+        [alert show];
+        
+    }
+    else
+    {
+        self.locationTracker = [LocationTracker sharedLocationTracker];
+        [self.locationTracker startLocationTracking];
+    }
+    
     return YES;
 }
 
---
+++ PathOGion/LocationTracker.h
@@ -0,0 +1,29 @@
+//
+//  LocationTracker.h
+//  Location
+//
+//  Created by Ricky Chea
+//  Copyright (c) 2014 Location. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+#import "BackgroundTaskManager.h"
+#define ACCURACY_TOLERANCE 100.0
+
+#define BEGIN_UPDATE_LOCATION_EVERY_N_SECONDS 15.0
+#define UPDATE_LOCATION_FOR_N_SECONDS 2.0
+
+@interface LocationTracker : NSObject <CLLocationManagerDelegate>
+
+@property (nonatomic) CLLocationCoordinate2D currentLocation;
+@property (nonatomic) CLLocationAccuracy currentLocationAccuracy;
+
++ (instancetype) sharedLocationTracker;
+
+- (void) startLocationTracking;
+- (void) stopLocationTracking;
+- (void) saveLocation;
+
+
+@end
