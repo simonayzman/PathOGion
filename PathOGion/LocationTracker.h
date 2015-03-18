3e7b8066df4d4c2d9191200c204689b3f48d55f0
Simon Ayzman <simonayzman@gmail.com>
2015-03-18 13:24:56 -0400

    Removed unnecessary #define's related to timers used previously

--- PathOGion/LocationTracker.h
+++ PathOGion/LocationTracker.h
@@ -15,9 +15,6 @@
 #define DISTANCE_FILTER 10.0
 #define ACCURACY_TOLERANCE 50.0
 
-#define BEGIN_UPDATE_LOCATION_EVERY_N_SECONDS 10.0
-#define UPDATE_LOCATION_FOR_N_SECONDS 1.0
-
 @interface LocationTracker : NSObject <CLLocationManagerDelegate>
 
 @property (strong, nonatomic) NSDictionary *currentLocation;
