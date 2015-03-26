5f99012b9921ccbd14d91f099ec14ad092fcdf95
Simon Ayzman <simonayzman@gmail.com>
2015-03-26 15:32:52 -0400

    Added initial POGMapViewController

---
+++ PathOGion/POGMapViewController.h
@@ -0,0 +1,16 @@
+//
+//  POGMapViewController.h
+//  PathOGion
+//
+//  Created by Simon Ayzman on 3/24/15.
+//  Copyright (c) 2015 CARSI Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface POGMapViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ PathOGion/POGMapViewController.m
@@ -0,0 +1,62 @@
+//
+//  POGMapViewController.m
+//  PathOGion
+//
+//  Created by Simon Ayzman on 3/24/15.
+//  Copyright (c) 2015 CARSI Lab. All rights reserved.
+//
+
+#import "POGMapViewController.h"
+
+@interface POGMapViewController ()
+
+@end
+
+@implementation POGMapViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setup];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+ Potentially do the navigation transparency here
+ 
+-(void)viewWillAppear:(BOOL)animated{
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setShadowImage:[UIImage new]];
+}
+
+-(void)viewWillDisappear:(BOOL)animated{
+    [self.navigationController.navigationBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
+    [self.navigationController.navigationBar setShadowImage:nil];
+}
+*/
+
+- (void) setup
+{
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+                                                  forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
+    self.navigationController.view.backgroundColor = [UIColor clearColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
