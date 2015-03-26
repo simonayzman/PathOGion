34dbd3656cd9cf32edca5c90e82c3715a8c0e3ca
Simon Ayzman <simonayzman@gmail.com>
2015-03-26 15:33:18 -0400

    Added initial POGSettingsViewController

---
+++ PathOGion/POGSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  POGSettingsViewController.h
+//  PathOGion
+//
+//  Created by Simon Ayzman on 3/24/15.
+//  Copyright (c) 2015 CARSI Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface POGSettingsViewController : UIViewController
+
+@end
