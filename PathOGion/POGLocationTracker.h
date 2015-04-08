dd4e156ec90dda2b97594d69e7adb9de931a8466
Simon Ayzman <simonayzman@gmail.com>
2015-04-07 23:47:22 -0400

    Updated accuracy tolerance for testing

--- PathOGion/POGLocationTracker.h
+++ PathOGion/POGLocationTracker.h
@@ -14,7 +14,7 @@
 @class POGLocationPoint;
 
 #define DISTANCE_FILTER 100.0
-#define ACCURACY_TOLERANCE 100.0
+#define ACCURACY_TOLERANCE 2000.0
 
 @interface POGLocationTracker : NSObject <CLLocationManagerDelegate>
 
