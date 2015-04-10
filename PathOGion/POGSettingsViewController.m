44aa31c0ffa6073a3b65951777cfa9e94b51348b
Simon Ayzman <simonayzman@gmail.com>
2015-04-09 22:19:23 -0400

    Added three segues to POGSettingsViewController

--- PathOGion/POGSettingsViewController.m
+++ PathOGion/POGSettingsViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "POGSettingsViewController.h"
+#import "POGSelectUserLocationPathViewController.h"
+#import "POGDeleteUserLocationPathViewController.h"
+#import "POGInfectedPatientLocationPathTableViewController.h"
 
 @interface POGSettingsViewController ()
 
@@ -16,7 +19,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self setup];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,14 +27,38 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+- (void) setup
+{
+
+}
+
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    
+    if ([[segue identifier] isEqualToString:@"selectUserLocationPathSetting"])
+    {
+        POGSelectUserLocationPathViewController *svc = [segue destinationViewController];
+    }
+    else if ([[segue identifier] isEqualToString:@"deleteUserLocationPathSetting"])
+    {
+        POGDeleteUserLocationPathViewController *dvc = [segue destinationViewController];
+
+    }
+    else if ([[segue identifier] isEqualToString:@"showInfectedPatientPaths"])
+    {
+        POGInfectedPatientLocationPathTableViewController *ptvc = [segue destinationViewController];
+    }
+    else
+    {
+        NSLog(@"Identifier: %@. Fatal error.", [segue identifier]);
+    }
+    
 }
-*/
+
 
 @end
